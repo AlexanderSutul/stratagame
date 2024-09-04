7a3046517855ba9eb34f5a879a639c21c40ea525
Aleksandr Sutulov <sutulovalexander@gmail.com>
2024-09-04 23:06:39 +0200

    add new target and formatting

---
+++ Stratogame Ios/Stratogame_IosApp.swift
@@ -0,0 +1,12 @@
+//
+
+import SwiftUI
+
+@main
+struct Stratogame_IosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- Stratogame Watch App/ViewModel.swift
+++ Stratogame Watch App/ViewModel.swift
@@ -38,57 +38,68 @@ let stratagems: [StratagemSection] = [
                   title: "Reinforce",
                   image: .reinforce,
                   description: "Amidst the chaos of battle, amidst the thunderous roar of conflict, we stand as one, united in purpose, bound by the sacred oath of liberty's champions. For every Helldiver who has fallen, for every comrade lost in the crucible of war, their memory shall be etched in the annals of heroism, their valor a beacon that guides us through the darkest of nights.",
-                  sequences: [.up, .down, .right, .left, .up]),
+                  sequences: [.up, .down, .right, .left, .up]
+              ),
               Stratagem(
                   title: "SOS Beacon",
                   image: .sos,
                   description: "The SOS Beacon is a Stratagem that deploys a beacon that calls other Helldivers in orbit to your aid.",
-                  sequences: [.up, .down, .left, .right]),
+                  sequences: [.up, .down, .left, .right]
+              ),
               Stratagem(
                   title: "Resupply",
                   image: .resupply,
                   description: "A pod containing ammunition and ordnance. The supplies take some time to deploy as they have to be customized before being deployed.",
-                  sequences: [.down, .down, .up, .right]),
+                  sequences: [.down, .down, .up, .right]
+              ),
               Stratagem(
                   title: "SEAF Artillery",
                   image: .seaf,
                   description: "S.E.A.F shells of varying types will spawn in several locations throughout a S.E.A.F fortification. There will always be at least 5 shells present but spawn-rates tend towards 7-8 shells.",
-                  sequences: [.right, .up, .up, .down]),
+                  sequences: [.right, .up, .up, .down]
+              ),
               Stratagem(
                   title: "Eagle Fighter",
                   image: .reeagle,
                   description: "The Eagle Fighter is a multi-role aerospace craft used by the Super Earth Armed Forces, often deployed under Helldiver Command to perform atmospheric operations.",
-                  sequences: [.up, .up, .left, .up, .right]),
+                  sequences: [.up, .up, .left, .up, .right]
+              ),
               Stratagem(
                   title: "NUX-223 Hellbomb",
                   image: .hell,
                   description: "The Hellbomb is provided when the Helldivers need to destroy a stationary target.",
-                  sequences: [.down, .up, .left, .down, .up, .right, .down, .up]),
+                  sequences: [.down, .up, .left, .down, .up, .right, .down, .up]
+              ),
               Stratagem(
                   title: "SSSD Delivery",
                   image: .ssd,
                   description: "No data, just delivery.",
-                  sequences: [.down, .down, .down, .up, .up]),
+                  sequences: [.down, .down, .down, .up, .up]
+              ),
               Stratagem(
                   title: "Upload Escape Pod Data",
                   image: .upload,
                   description: "An escape pod with information vital to the war effort has crashed on the surface of this planet. Your job is to collect that data. Dive in, find the pod, and upload the data using yourself as a relay. Now get down there, helldiver.",
-                  sequences: [.left, .right, .up, .up, .up]),
+                  sequences: [.left, .right, .up, .up, .up]
+              ),
               Stratagem(
                   title: "Seismic Probe",
                   image: .probe,
                   description: "Just no data.",
-                  sequences: [.up, .up, .left, .right, .down, .down]),
+                  sequences: [.up, .up, .left, .right, .down, .down]
+              ),
               Stratagem(
                   title: "Prospecting Drill",
                   image: .drill,
                   description: "Just no data.",
-                  sequences: [.down, .down, .left, .right, .down, .down]),
+                  sequences: [.down, .down, .left, .right, .down, .down]
+              ),
               Stratagem(
                   title: "Spread Democracy",
                   image: .flag,
                   description: "Raise the Super Earth flag as an unassailable beacon of Liberty.",
-                  sequences: [])
+                  sequences: []
+              ),
 
           ]),
     .init(type: .SupplyBackpacks,
@@ -116,6 +127,6 @@ let stratagems: [StratagemSection] = [
               .init(title: "AX/AR-23 \"Guard Dog\"",
                     image: .guarddog,
                     description: "The AD-334 Guard Dog is a defensive automated drone armed with a small submachine gun. It is based from a backpack carried on the Helldivers back and will land there to rearm whenever it runs out of ammunition. The AD-334 is part of the Defender equipment kit.",
-                    sequences: [.down, .up, .left, .up, .right, .down])
-          ])
+                    sequences: [.down, .up, .left, .up, .right, .down]),
+          ]),
 ]
